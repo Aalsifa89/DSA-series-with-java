00feb723c6062f6a0d5653917ed0cc509b0d1c1d
Aalsifa89 <alsifajahan5@gmail.com>
2026-04-06 13:07:52 +0530

    Add files via upload

---
+++ indexarray.java
@@ -0,0 +1,14 @@
+public class indexarray {
+    public static void main(String[] args){
+        System.out.println();
+int d = sc.nextInt();
+int index = -1;
+for(int i=0;i<n;i++){
+    if(arr[i]== d){
+        index = i;
+        break;
+    }
+}
+System.out.println(index);
+    }
+}
